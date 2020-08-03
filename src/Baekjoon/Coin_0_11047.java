f890198aa2d34e18bd988e8d782d370d0fb6b0f5
LeeGeumLak <ekdma4105@naver.com>
2020-08-03 14:23:09 +0900

    그리디 알고리즘

---
+++ src/Baekjoon/Coin_0_11047.java
@@ -0,0 +1,51 @@
+package Baekjoon;
+
+import java.util.Scanner;
+
+public class Coin_0_11047 {
+    public static void main(String[] args) {
+        Scanner inputParameter = new Scanner(System.in);
+
+        int coinTypeNum = 0;
+        int valueAmount = 0;
+
+        coinTypeNum = inputParameter.nextInt();
+        valueAmount = inputParameter.nextInt();
+
+        int[] coinValueArray = new int[coinTypeNum];
+
+        for(int i = 0; i < coinTypeNum; i++) {
+            coinValueArray[i] = inputParameter.nextInt();
+        }
+
+
+        System.out.println( calMinCoinNum(coinTypeNum, valueAmount, coinValueArray) );
+    }
+
+    public static int calMinCoinNum(int coinTypeNum, int valueAmount, int[] coinValueArray) {
+        int minCoinNum = 0;
+        int maxIndex = coinTypeNum;
+
+        for(int i = 0; i < coinTypeNum; i++) {
+            if(coinValueArray[i] > valueAmount) {
+                maxIndex = i-1;
+
+                if(maxIndex < 0) {
+                    System.exit(0);
+                }
+
+                break;
+            }
+        }
+
+        for(int i = maxIndex; i >= 0; i--) {
+            while(valueAmount >= coinValueArray[i]) {
+                minCoinNum++;
+                valueAmount -= coinValueArray[i];
+            }
+        }
+
+        return minCoinNum;
+    }
+
+}
