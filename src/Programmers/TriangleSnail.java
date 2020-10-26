4a5b644fd4ba36efaa4dee43604185d43b0c05af
leegeumlak <ekdma410562@gmail.com>
2020-10-26 17:20:38 +0900

    -ing

--- src/Programmers/TriangleSnail.java
+++ src/Programmers/TriangleSnail.java
@@ -1,12 +1,14 @@
 package Programmers;
 
 public class TriangleSnail {
-    public static int calculateFactorial(int n) {
+    // 실행시간이 너무 오래 걸림
+    /*
+    public static int calculateTotalSum(int n) {
         if (n <= 1) {
             return n;
         }
         else {
-            return calculateFactorial(n - 1) * n;
+            return calculateTotalSum(n - 1) * n;
         }
     }
 
@@ -15,19 +17,60 @@ public class TriangleSnail {
         // 출력 : 높이가 n인 삼각형
         //       단, 맨 위 꼭지점부터 반시계 방향으로 숫자를 채우고
         //           출력은 위에서 아래 순서대로
-        /* 예시
-                1
-              2   9
-            3   10  8
-          4   5   6   7
-        */
+        //       예시
+        //                1
+        //              2   9
+        //            3   10  8
+        //          4   5   6   7
 
+        int[][] triangleArray = new int[n][n];
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < n; j++) {
+                triangleArray[i][j] = -1;
+            }
+        }
+
+        int totalSumN = calculateTotalSum(n);
+        int[] answer = new int[totalSumN];
+
+        int i = 0, j = 0, k = 1;
+        triangleArray[i][j] = k;
+
+        // 삼각형 배열 숫자 입력
+        while (k < totalSumN) {
+            // 왼쪽 변
+            while (i + 1 < n && k < totalSumN && triangleArray[i + 1][j] < 0) {
+                triangleArray[++i][j] = ++k;
+            }
+
+            // 아래쪽 변
+            while (j + 1 < n && k < totalSumN && triangleArray[i][j + 1] < 0) {
+                triangleArray[i][++j] = ++k;
+            }
+
+            // 오른쪽 변
+            while (i - 1 > 0 && k < totalSumN && triangleArray[i - 1][j - 1] < 0) {
+                triangleArray[--i][--j] = ++k;
+            }
+        }
+
+        k = 0;
+
+        // 위에서 아래 순서대로 answer 배열에 입력
+        for (i = 0; i < n; i++) {
+            for (j = 0; j <= i; j++) {
+                answer[k++] = triangleArray[i][j];
+            }
+        }
+
+        return answer;
+    }
+    */
+
+    public int[] solution(int n) {
         int[] answer = {};
 
-        int[] triangleArray = new int[calculateFactorial(n)];
 
-        // ( 아래로 높이 n 만큼 -> 가로로 길이 n-1 만큼 -> 위로 높이 n-2 만큼 ) 반복
-        
 
         return answer;
     }
