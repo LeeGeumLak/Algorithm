a6b2480e38ef422412153c15fd447c0d89734f2a
leegeumlak <ekdma410562@gmail.com>
2020-10-27 12:16:02 +0900

    프로그래머스 lv2 - 삼각 달팽이

--- src/Programmers/TriangleSnail.java
+++ src/Programmers/TriangleSnail.java
@@ -62,5 +62,38 @@ public class TriangleSnail {
         }
 
         return answer;
+
+        /*
+        // cool answer
+        int[] answer = new int[(n*(n+1))/2];
+        int[][] matrix = new int[n][n];
+
+        int x = -1, y = 0;
+        int num = 1;
+
+        for (int i = 0; i < n; ++i) {
+            for (int j = i; j < n; ++j) {
+                if (i % 3 == 0) {
+                    ++x;
+                } else if (i % 3 == 1) {
+                    ++y;
+                } else if (i % 3 == 2) {
+                    --x;
+                    --y;
+                }
+                matrix[x][y] = num++;
+            }
+        }
+
+        int k = 0;
+        for(int i = 0; i < n; ++i) {
+            for(int j = 0; j < n; ++j) {
+                if(matrix[i][j] == 0) break;
+                answer[k++] = matrix[i][j];
+            }
+        }
+
+        return answer;
+         */
     }
 }
