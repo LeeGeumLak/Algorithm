aabcec45da1aa6b7375918cab4b8d4b8d9dfc4b0
leegeumlak <ekdma410562@gmail.com>
2020-10-27 15:21:21 +0900

    프로그래머스 lv2 - 큰 수 만들기

--- src/Programmers/MakingBigNumber.java
+++ src/Programmers/MakingBigNumber.java
@@ -27,5 +27,24 @@ public class MakingBigNumber {
         }
 
         return answer.toString();
+
+
+        // cool answer
+        /*
+        char[] result = new char[number.length() - k];
+        Stack<Character> stack = new Stack<>();
+
+        for (int i=0; i<number.length(); i++) {
+            char c = number.charAt(i);
+            while (!stack.isEmpty() && stack.peek() < c && k-- > 0) {
+                stack.pop();
+            }
+            stack.push(c);
+        }
+        for (int i=0; i<result.length; i++) {
+            result[i] = stack.get(i);
+        }
+        return new String(result);
+         */
     }
 }
