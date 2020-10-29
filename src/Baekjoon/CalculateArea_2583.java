09c2823578139c1b59961d3a4840573d359aa159
leegeumlak <ekdma410562@gmail.com>
2020-10-29 16:24:24 +0900

    DFS

--- src/Baekjoon/CalculateArea_2583.java
+++ src/Baekjoon/CalculateArea_2583.java
@@ -1,15 +1,19 @@
 package Baekjoon;
 
+import java.util.PriorityQueue;
 import java.util.Scanner;
 
-//DFS
 public class CalculateArea_2583 {
     static int M, N, K;
     static int[][] plottingPaper;
     static boolean[][] visited;
 
-    static int areaCount = 0;
-    static int[] areaSize;
+    static int[] dx = {1, -1, 0, 0};
+    static int[] dy = {0, 0, 1, -1};
+
+    static int areaCount;
+    static PriorityQueue<Integer> areaSizeQueue = new PriorityQueue<Integer>();
+
 
     public static void main(String[] args) {
         // M x N 모눈종이 -> K 개의 직사각형 그리기
@@ -33,22 +37,45 @@ public class CalculateArea_2583 {
             int squareX_2 = inputParameter.nextInt();
             int squareY_2 = inputParameter.nextInt();
 
-            for(int j = squareX_1; j <= squareX_2; j++) {
-                for(int k = squareY_1; k <= squareY_2; k++) {
+            for(int j = squareY_1; j < squareY_2; j++) {
+                for(int k = squareX_1; k < squareX_2; k++) {
                     plottingPaper[j][k] = 1;
                 }
             }
         }
 
+
         for(int i = 0; i < M; i++) {
             for(int j = 0; j < N; j++) {
-
+                areaCount = 0;
+                if(plottingPaper[i][j] == 0) {
+                    dfs(i, j);
+                    areaSizeQueue.offer(areaCount);
+                }
             }
         }
 
+        System.out.println(areaSizeQueue.size());
+        while(!areaSizeQueue.isEmpty()) {
+            System.out.print(areaSizeQueue.poll() + " ");
+        }
     }
 
-    public static void dfs() {
+    public static void dfs(int x, int y) {
+        plottingPaper[x][y] = 1;
+        areaCount++;
 
+        for(int i = 0; i < 4; i++) {
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+
+
+            if(nx >= 0 && nx < M && ny >= 0 && ny < N) {
+                if(plottingPaper[nx][ny] == 0) {
+                    dfs(nx, ny);
+                }
+            }
+
+        }
     }
 }
