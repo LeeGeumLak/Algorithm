03b487062ab0ce52280fdd59a461a806bf8e58e3
leegeumlak <ekdma410562@gmail.com>
2020-11-10 16:00:40 +0900

    타겟 넘버 - ing

---
+++ src/Programmers/TargetNumber.java
@@ -0,0 +1,19 @@
+package Programmers;
+
+// dfs/ bfs
+public class TargetNumber {
+    public int solution(int[] numbers, int target) {
+        // n개의 음이 아닌 정수, 이 수를 적절히 더하거나 빼서 다켓 넘버를 만들고자 함
+        // 사용할 수 있는 숫자가 담긴 배열로 타겟 넘버를 만들 수 있는 방법의 수는?
+
+        int answer = 0;
+
+
+
+        return answer;
+    }
+
+    public static void dfs() {
+
+    }
+}
